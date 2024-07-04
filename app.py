5d64e734da76c74dbbd3b5b922a5b9f2a7b5ea88
codeforfunny <117640162+codeforfunny@users.noreply.github.com>
2024-07-04 11:34:39 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,14 @@
-from flask import Flask, render_template, request
-import google.generativeai as palm
-import os
-
-api_key = os.getenv("MAKERSUITE_API_TOKEN")
-palm.configure(api_key=api_key)
-
-model = {"model": "models/chat-bison-001"}
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET","POST"])
+@app.route("/", methods=["GET"])
 def index():
-    return(render_template("index.html"))
+    return render_template("index.html")
 
-@app.route("/main", methods=["GET","POST"])
-def main():
-    r = request.form.get("q")
-    print(r)
-    return(render_template("main.html",r=r))
-
-@app.route("/traffic_thailand", methods=["GET","POST"])
-def traffic_thailand():
-    q = "thailand traffic"
-    r = palm.chat(**model, messages=q)
-    return(render_template("traffic_thailand.html",r=r.last))
+@app.route("/categories/<category>", methods=["GET"])
+def category_page(category):
+    return render_template(f"{category}.html")
 
 if __name__ == "__main__":
-    app.run() 
+    app.run(debug=True)
